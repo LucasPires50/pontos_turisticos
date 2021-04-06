ff85fae243fe488c9db0b99dca6fb0bdaf7d8c0f
LucasPires50 <luc_50@outlook.com>
2021-04-06 15:06:01 -0300

    Implementando imagens na API

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -16,22 +16,24 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
-        return Response({'teste': 123})
+        # return Response({'teste': 123})
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-        return Response({'Hello Word': request.data['nome']})
+        # return Response({'Hello Word': request.data['nome']})
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        pass
+         return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+         return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        pass
+         return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
-        pass
+         return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
     @action(methods=['post', 'get'], detail=True)
     def denunciar(self, request, pk=None):
