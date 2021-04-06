8936b9bc8f9d5a8d2012963daad716d4d954fa3b
LucasPires50 <luc_50@outlook.com>
2021-04-06 16:56:53 -0300

    Utilizando o SearchFilter

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -9,50 +10,52 @@ class PontoTuristicoViewSet(ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    
+    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['nome', 'descricao', 'endereco__linha1']
 
-    def get_queryset(self):
-        id = self.request.query_params.get('id', None)
-        nome = self.request.query_params.get('nome', None)
-        descricao = self.request.query_params.get('descricao', None)
-        queryset = PontoTuristico.objects.all()
+    # def get_queryset(self):
+    #     id = self.request.query_params.get('id', None)
+    #     nome = self.request.query_params.get('nome', None)
+    #     descricao = self.request.query_params.get('descricao', None)
+    #     queryset = PontoTuristico.objects.all()
 
-        if id:
-            queryset = queryset = PontoTuristico.objects.filter(pk=id)
+    #     if id:
+    #         queryset = queryset = PontoTuristico.objects.filter(pk=id)
 
-        if nome: 
-            queryset = queryset.filter(nome__iexact=nome)
+    #     if nome: 
+    #         queryset = queryset.filter(nome__iexact=nome)
 
-        if descricao: 
-            queryset = queryset.filter(descricao__iexact=descricao)
+    #     if descricao: 
+    #         queryset = queryset.filter(descricao__iexact=descricao)
 
-        return queryset
+    #     return queryset
 
-    def list(self, request, *args, **kwargs):
-        # return Response({'teste': 123})
-        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+    # def list(self, request, *args, **kwargs):
+    #     # return Response({'teste': 123})
+    #     return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
-    def create(self, request, *args, **kwargs):
-        # return Response({'Hello Word': request.data['nome']})
-        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+    # def create(self, request, *args, **kwargs):
+    #     # return Response({'Hello Word': request.data['nome']})
+    #     return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
-    def destroy(self, request, *args, **kwargs):
-         return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
+    # def destroy(self, request, *args, **kwargs):
+    #      return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
-    def retrieve(self, request, *args, **kwargs):
-         return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+    # def retrieve(self, request, *args, **kwargs):
+    #      return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
-    def update(self, request, *args, **kwargs):
-         return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+    # def update(self, request, *args, **kwargs):
+    #      return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
-    def partial_update(self, request, *args, **kwargs):
-         return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
+    # def partial_update(self, request, *args, **kwargs):
+    #      return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
-    @action(methods=['post', 'get'], detail=True)
-    def denunciar(self, request, pk=None):
-        pass
+    # @action(methods=['post', 'get'], detail=True)
+    # def denunciar(self, request, pk=None):
+    #     pass
     
-    @action (methods='get', detail=False)
-    def teste(self, request):
-        pass
+    # @action (methods='get', detail=False)
+    # def teste(self, request):
+    #     pass
