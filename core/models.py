71c286a85319744c31342ec351d0377ae7a53630
root <root@leader-system-product-name.local>
2021-04-06 08:19:36 -0300

    Subindo o projeto para o GitHub

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from atracoes.models import Atracao
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
+
+# criando o model
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao) # acoplamento
+    comentarios = models.ManyToManyField(Comentario) # acoplamento
+    avaliacoes = models.ManyToManyField(Avaliacao) # acoplamento
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nome
+
