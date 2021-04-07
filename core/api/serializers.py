ff3f0b129e961ccc6ef758163d3b53a509926b10
LucasPires50 <luc_50@outlook.com>
2021-04-07 17:23:26 -0300

    Foreignkeys relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,24 +1,44 @@
-from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from core.models import PontoTuristico
+from rest_framework.serializers import ModelSerializer
+from core.models import PontoTuristico, Atracao, Endereco
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(ModelSerializer):
-    atracoes = AtracaoSerializer(
-        many=True, read_only=True)  # Mult-relacinamentos
-    endereco = EnderecoSerializer(read_only=True)  # Relacinamento Simples
-    descricao_completa = serializers.SerializerMethodField()
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
-        fields = (
-            'id', 'nome', 'descricao', 'foto', 'atracoes', 'comentarios',
-                  'avaliacoes', 'endereco', 'descricao_completa', 'descricao_completa2'
-        )
-        read_only_fields = ['comentarios', 'avaliacoes']
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto','atracoes', 'comentarios', 'avaliacoes', 'endereco',
+                  'descricao_completa', 'descricao_completa2'
+                  )
+        read_only_fields = ('comentarios', 'avaliacoes')
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+
+        return ponto
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
