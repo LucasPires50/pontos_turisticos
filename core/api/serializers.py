4c1f09220dac66f6c1d940ca06ff7ed051745ecc
LucasPires50 <luc_50@outlook.com>
2021-04-07 15:34:27 -0300

    relacionamentos de campos (read_only)

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,13 +5,20 @@ from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 
+
 class PontoTuristicoSerializer(ModelSerializer):
-    atracoes = AtracaoSerializer(many=True)# Mult-relacinamentos
-    endereco = EnderecoSerializer()# Relacinamento Simples
+    atracoes = AtracaoSerializer(
+        many=True, read_only=True)  # Mult-relacinamentos
+    endereco = EnderecoSerializer(read_only=True)  # Relacinamento Simples
     descricao_completa = serializers.SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'endereco', 'descricao_completa', 'descricao_completa2')
+        fields = (
+            'id', 'nome', 'descricao', 'foto', 'atracoes', 'comentarios',
+                  'avaliacoes', 'endereco', 'descricao_completa', 'descricao_completa2'
+        )
+        read_only_fields = ['comentarios', 'avaliacoes']
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
