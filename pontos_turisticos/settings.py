4f372899680f311fa6f0140a82b37d148cc746d9
LucasPires50 <luc_50@outlook.com>
2021-04-07 13:37:24 -0300

    deploy heroku

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos3.herokuapp.com/', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-turisticos4.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
