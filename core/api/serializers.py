3cbdd59a48b23206ae10ec1cd7a1b8084c9cefc5
LucasPires50 <luc_50@outlook.com>
2021-04-07 09:13:02 -0300

    SerializerMethodField

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
@@ -6,6 +8,10 @@ from enderecos.api.serializers import EnderecoSerializer
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)# Mult-relacinamentos
     endereco = EnderecoSerializer()# Relacinamento Simples
+    descricao_completa = serializers.SerializerMethodField()
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'endereco')
+        fields = ('id', 'nome', 'descricao', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'endereco', 'descricao_completa')
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
