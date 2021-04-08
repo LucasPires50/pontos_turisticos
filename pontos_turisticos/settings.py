eb184e02e060fca8b159e79524fc16196ab75379
LucasPires50 <luc_50@outlook.com>
2021-04-08 10:19:32 -0300

    habilitando paginação

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -136,3 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
