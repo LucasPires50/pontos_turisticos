ed62d8cc7ea5ed5bb56b5677a0f153ded961a07a
LucasPires50 <luc_50@outlook.com>
2021-04-08 10:31:49 -0300

    Aprovando e reprovando comentarios com Actions personalizadas

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Comentario
+from .actions import reprova_comentarios, aprova_comentarios
 
-admin.site.register(Comentario)
+# Implementar ao class actions admin do comentatios
+class ComentatioAdmin(admin.ModelAdmin):
+    # Mostar os campos na tela do django admin
+    list_display=['usuario', 'data', 'aprovado']
+    # Mostra as actons
+    actions = [reprova_comentarios, aprova_comentarios]
+
+admin.site.register(Comentario, ComentatioAdmin)
